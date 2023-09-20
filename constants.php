157ecfa56905733f9fe531f59e2f6349e68ee489
Tsemaye Michael Ikomi <113984783+ikomi22@users.noreply.github.com>
2023-09-20 14:55:09 +0100

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,11 @@
+<?php
+//Start session
+session_start();
+//Create constants to save Database Cridentials
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'Task_manager');
+
+define('SITEURL', 'https://mytaskmanager-hazel.vercel.app/');
+?>
